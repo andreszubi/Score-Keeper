8f9c8506df220d32873e764d8ff01d3891d1dc0b
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2026-02-20 12:11:41 -0500

    Initializing project

---
+++ Score Keeper/Score Keeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Score Keeper
+//
+//  Created by Andres Zubizarreta on 2/20/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Score Keeper/Score Keeper/Score_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Score_KeeperApp.swift
+//  Score Keeper
+//
+//  Created by Andres Zubizarreta on 2/20/2026.
+//
+
+import SwiftUI
+
+@main
+struct Score_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
